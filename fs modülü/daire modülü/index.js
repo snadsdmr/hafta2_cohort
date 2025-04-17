8654fbd991264854ec7af11e7db8ae9cf35c9b46
Sena Daşdemir <79470341+snadsdmr@users.noreply.github.com>
2025-04-17 22:36:27 +0300

    Delete fs modülü/daire modülü directory

--- fs modülü/daire modülü/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-
-const { circleArea, circleCircumference } = require('./circle');
-
-const r = 5;
-
-console.log(`Yarıçapı ${r} olan dairenin alanı: ${circleArea(r).toFixed(2)}`);
-console.log(`Yarıçapı ${r} olan dairenin çevresi: ${circleCircumference(r).toFixed(2)}`);
